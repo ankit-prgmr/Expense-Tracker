ef5aa963a9f856052ca8d1f953f3e9909b090d47
ankit <ankitagarwal912011@gmail.com>
2020-04-22 12:14:38 +0530

    initial state change

---
+++ src/components/TransactionList.js
@@ -0,0 +1,20 @@
+import React, { useContext } from "react";
+import { GlobalContext } from "../context/GlobalState";
+import Transaction from "./Transaction";
+
+const TransactionList = (props) => {
+  const { transactions } = useContext(GlobalContext);
+
+  return (
+    <>
+      <h3>History</h3>
+      <ul className="list">
+        {transactions.map((transaction) => {
+          return <Transaction transaction={transaction} key={transaction.id} />;
+        })}
+      </ul>
+    </>
+  );
+};
+
+export default TransactionList;
---
+++ src/context/GlobalState.js
@@ -0,0 +1,44 @@
+import React, { useReducer } from "react";
+import AppReducer from "./AppReducer";
+
+//Initial State
+const initialState = {
+  transactions: [],
+};
+
+//create context
+
+export const GlobalContext = React.createContext(initialState);
+
+//Provider Component
+
+export const GlobalProvider = ({ children }) => {
+  const [state, dispatch] = useReducer(AppReducer, initialState);
+
+  //Actions
+  function deleteTransaction(id) {
+    dispatch({
+      type: "DELETE_TRANSACTION",
+      payload: id,
+    });
+  }
+
+  function addTransaction(transaction) {
+    dispatch({
+      type: "ADD_TRANSACTION",
+      payload: transaction,
+    });
+  }
+
+  return (
+    <GlobalContext.Provider
+      value={{
+        transactions: state.transactions,
+        deleteTransaction,
+        addTransaction,
+      }}
+    >
+      {children}
+    </GlobalContext.Provider>
+  );
+};
